8193d17ffcd9e35b047a919f03ca162a1cfc1cbf
dev.Rodrigo Niskier <niskier.rodrigo@gmail.com>
2026-08-24 23:00:20 -0300

    Usar novamente os três módulos do acervo

--- src/dados/biblioteca-completa.ts
+++ src/dados/biblioteca-completa.ts
@@ -1,15 +1,9 @@
 import { biblioteca as bibliotecaPrincipal } from './biblioteca';
 import { bibliotecaExtra } from './biblioteca-extra';
 import { bibliotecaFinal } from './biblioteca-final';
-import { bibliotecaSeculo20 } from './biblioteca-seculo20';
 
 /** Acervo completo consumido pela interface. */
-export const biblioteca = [
-  ...bibliotecaPrincipal,
-  ...bibliotecaExtra,
-  ...bibliotecaFinal,
-  ...bibliotecaSeculo20,
-];
+export const biblioteca = [...bibliotecaPrincipal, ...bibliotecaExtra, ...bibliotecaFinal];
 
 export const porDisciplina = (codigo: string) =>
   biblioteca.filter((o) => o.disciplinas.includes(codigo));
