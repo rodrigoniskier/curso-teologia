ea9e6d4a5ed2276ce250b538650f6a4c1ef710e8
dev.Rodrigo Niskier <niskier.rodrigo@gmail.com>
2026-08-24 22:53:11 -0300

    Incluir acervo documental do século XX

--- src/dados/biblioteca-completa.ts
+++ src/dados/biblioteca-completa.ts
@@ -1,9 +1,15 @@
 import { biblioteca as bibliotecaPrincipal } from './biblioteca';
 import { bibliotecaExtra } from './biblioteca-extra';
 import { bibliotecaFinal } from './biblioteca-final';
+import { bibliotecaSeculo20 } from './biblioteca-seculo20';
 
 /** Acervo completo consumido pela interface. */
-export const biblioteca = [...bibliotecaPrincipal, ...bibliotecaExtra, ...bibliotecaFinal];
+export const biblioteca = [
+  ...bibliotecaPrincipal,
+  ...bibliotecaExtra,
+  ...bibliotecaFinal,
+  ...bibliotecaSeculo20,
+];
 
 export const porDisciplina = (codigo: string) =>
   biblioteca.filter((o) => o.disciplinas.includes(codigo));
