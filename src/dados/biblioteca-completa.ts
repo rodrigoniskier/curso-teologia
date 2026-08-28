10b26939e3c1b1e9d528419981219dec343682f0
dev.Rodrigo Niskier <niskier.rodrigo@gmail.com>
2026-08-27 23:38:17 -0300

    Integrar fontes dos ciclos de aprofundamento ao acervo

--- src/dados/biblioteca-completa.ts
+++ src/dados/biblioteca-completa.ts
@@ -1,9 +1,15 @@
 import { biblioteca as bibliotecaPrincipal } from './biblioteca';
 import { bibliotecaExtra } from './biblioteca-extra';
 import { bibliotecaFinal } from './biblioteca-final';
+import { bibliotecaAprofundamento } from './biblioteca-aprofundamento';
 
 /** Acervo completo consumido pela interface. */
-export const biblioteca = [...bibliotecaPrincipal, ...bibliotecaExtra, ...bibliotecaFinal];
+export const biblioteca = [
+  ...bibliotecaPrincipal,
+  ...bibliotecaExtra,
+  ...bibliotecaFinal,
+  ...bibliotecaAprofundamento,
+];
 
 export const porDisciplina = (codigo: string) =>
   biblioteca.filter((o) => o.disciplinas.includes(codigo));
