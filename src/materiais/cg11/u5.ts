a3a47ecd3d0e1e5c5b0f0666aa3b8bf34300cf8c
dev.Rodrigo Niskier <niskier.rodrigo@gmail.com>
2026-09-14 12:01:46 -0300

    CG11: iniciar unidade 5

---
+++ src/materiais/cg11/u5.ts
@@ -0,0 +1,13 @@
+import type { MaterialUnidade } from '../../tipos';
+
+export const material: MaterialUnidade = {
+  id: 'cg11-u5-cultura-virtual-espiritualidade',
+  disciplina: 'CG11',
+  unidade: 5,
+  titulo: 'Cultura virtual e espiritualidade',
+  objetivo: 'Analisar como ambientes digitais reorganizam presença, identidade, autoridade, comunidade e práticas de espiritualidade.',
+  topicosCobertos: [],
+  blocos: [],
+  fontes: [],
+  atualizadoEm: '2026-09-14',
+};
